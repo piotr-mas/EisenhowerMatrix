1cd7829a45968e7dfb6da8b657541670797d9aea
piotr-mas <piotr.masior@outlook.com>
2025-07-09 18:40:55 +0200

    Initial commit

---
+++ gateway/src/test/java/com/piotr/matrix/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piotr.matrix.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
